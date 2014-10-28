0afe926d67e935d4587f910d2ab8c0ee5d97635d
Samuel Kirkpatrick <samuel.kirkpatrick@outlook.com>
2014-10-28 14:41:43 +1100

    Test push to test modulus

---
+++ dist/server/server.js
@@ -0,0 +1,18 @@
+var restify = require('restify');
+
+var port = process.env.PORT || 8080;
+
+function respond(req, res, next) {
+    res.send('hello ' + req.params.name);
+    next();
+}
+
+var server = restify.createServer();
+server.get('/hello/:name', respond);
+server.head('/hello/:name', respond);
+
+server.listen(port, function() {
+    console.log('%s listening at %s', server.name, server.url);
+});
+
+
